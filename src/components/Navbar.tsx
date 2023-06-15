50d6d12ef3c0a4a1bc9fb99b7849d3330b74140a
Arjun Sivaraman <arjun140702@gmail.com>
2023-06-16 00:46:44 +0530

    Add functionality to avatar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,47 @@
 import { Image } from '@chakra-ui/next-js'
-import { HStack, Spacer, Avatar } from '@chakra-ui/react'
+import { HStack, Spacer, Avatar, Text } from '@chakra-ui/react'
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuGroup,
+  MenuItem,
+  MenuDivider
+} from '@chakra-ui/react'
 import { InputGroup, InputLeftElement, Input } from '@chakra-ui/react'
 import Logo from '@/assets/images/logo.svg'
 import { FaSearch } from 'react-icons/fa'
 
 import { signOut, useSession } from 'next-auth/react'
+
+const AvatarMenu = ({
+  user
+}: {
+  user: { name?: string | null; image?: string | null; email?: string | null }
+}) => {
+  return (
+    <Menu>
+      <MenuButton
+        as={Avatar}
+        size="md"
+        name={user?.name!}
+        src={user?.image!}
+        cursor="pointer"
+      />
+      <MenuList>
+        <MenuGroup title="Signed in as" my={0} fontWeight="medium">
+          <Text pl={4} fontWeight="semibold" fontSize="sm">
+            {user?.email}
+          </Text>
+        </MenuGroup>
+        <MenuDivider />
+        <MenuItem onClick={() => signOut({ callbackUrl: '/' })}>
+          Logout
+        </MenuItem>
+      </MenuList>
+    </Menu>
+  )
+}
 
 export default function Navbar({ type }: { type?: 'light' | 'dark' }) {
   const { data: session } = useSession()
@@ -21,14 +58,7 @@ export default function Navbar({ type }: { type?: 'light' | 'dark' }) {
         <Input w="33%" placeholder="Search" />
       </InputGroup>
       <Spacer />
-      <Avatar
-        size="md"
-        name={session?.user?.name!}
-        src={session?.user?.image!}
-        cursor="pointer"
-        onClick={() => signOut({ callbackUrl: '/' })}
-      />
-      )
+      <AvatarMenu user={session?.user!} />)
     </HStack>
   )
 }
