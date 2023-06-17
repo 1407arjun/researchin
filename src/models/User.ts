ac5a716c2d1d622229a3b8cfbb3edb9f038293cd
Arjun Sivaraman <arjun140702@gmail.com>
2023-06-18 00:22:34 +0530

    Add auto timestamps

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose'
 import { AdapterUser } from 'next-auth/adapters'
 
-export const userSchema = new mongoose.Schema<AdapterUser>({
-  name: String,
-  image: String,
-  email: String,
-  emailVerified: Date
-})
+export const userSchema = new mongoose.Schema<AdapterUser>(
+  {
+    name: String,
+    image: String,
+    email: String,
+    emailVerified: Date
+  },
+  { timestamps: true }
+)
 
 export default mongoose.models.User ||
   mongoose.model<AdapterUser>('User', userSchema)
